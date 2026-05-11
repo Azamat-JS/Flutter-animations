c5db5620afb181f2674c1fc24c524890c5091020
Azamat-JS <azamatielts987@gmail.com>
2026-05-11 19:36:02 +0500

    implemented list animation

--- lib/explicit_examples/list_animation.dart
+++ lib/explicit_examples/list_animation.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ListAnimation extends StatelessWidget {
+class ListAnimation extends StatefulWidget {
   const ListAnimation({super.key});
+
+  @override
+  State<ListAnimation> createState() => _ListAnimationState();
+}
+
+class _ListAnimationState extends State<ListAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late List<Animation<Offset>> positionAnimations;
+  final itemCount = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    positionAnimations = List.generate(
+        itemCount,
+        (index) => Tween(begin: const Offset(-1, 0), end: Offset.zero)
+            .animate(controller));
+
+    Tween(begin: const Offset(-1, 0), end: Offset.zero).animate(controller);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +36,20 @@ class ListAnimation extends StatelessWidget {
         title: const Text('List Animation'),
       ),
       body: ListView.builder(
-        itemCount: 5,
+        itemCount: itemCount,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Salom Dunyo!, Azamat. ${index.toString()}'),
+          return SlideTransition(
+            position: positionAnimations[index],
+            child: ListTile(
+              title: Text('Salom Dunyo!, Azamat. ${index.toString()}'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          controller.forward();
+        },
         child: const Icon(Icons.done),
       ),
     );
